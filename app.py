9a815521339408a99afc7b1a1e48991aaffb9c7e
P104 <zequinteiro10@gmail.com>
2019-06-04 17:52:34 +0100

    site

--- app.py
+++ app.py
@@ -1,11 +1,32 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 
-@app.route('/')
+def gravar(v1, v2, v3):
+    import sqlite3
+    ficheiro = sqlite3.connect('db/Utilizadores.db')
+    db = ficheiro.cursor()
+    db.execute("CREATE TABLE IF NOT EXISTS usr (usr txt, pwd txt)")
+    db.execute("INSERT INTO usr VALUES (?,?)",(v1, v2) )
+    ficheiro.commit()
+    ficheiro.close()
+    return
+
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('registo.html')
+    erro = None
+    if request.method == 'POST':
+       v1= request.form['usr']
+       v2= request.form['pwd']
+       v3= request.form['cpwd']
+       if v2 != v3:
+           erro= 'A palavra passe não coincide.'
+       else:
+           gravar (v1, v2, v3)
+    return render_template('registo.html',erro=erro )
+
 
 
 if __name__ == '__main__':
