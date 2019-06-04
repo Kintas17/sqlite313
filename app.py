f99d23c3a95e37d2355cdf7bc9a882af00e768a4
P104 <zequinteiro10@gmail.com>
2019-06-04 18:25:20 +0100

    4 jun

--- app.py
+++ app.py
@@ -3,12 +3,23 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 
 
-def gravar(v1, v2, v3):
+def gravar(v1, v2):
     import sqlite3
     ficheiro = sqlite3.connect('db/Utilizadores.db')
     db = ficheiro.cursor()
     db.execute("CREATE TABLE IF NOT EXISTS usr (usr txt, pwd txt)")
-    db.execute("INSERT INTO usr VALUES (?,?)",(v1, v2) )
+    db.execute("INSERT INTO usr VALUES (?,?)", (v1, v2))
+    ficheiro.commit()
+    ficheiro.close()
+    return
+
+
+def alterar(v1, v2):
+    import sqlite3
+    ficheiro = sqlite3.connect('db/Utilizadores.db')
+    db = ficheiro.cursor()
+    db.execute("CREATE TABLE IF NOT EXISTS usr (usr txt, pwd txt)")
+    db.execute("UPDATE usr SET pwd = ? WHERE usr = ?", (v2, v1))
     ficheiro.commit()
     ficheiro.close()
     return
@@ -18,15 +29,14 @@ def gravar(v1, v2, v3):
 def index():
     erro = None
     if request.method == 'POST':
-       v1= request.form['usr']
-       v2= request.form['pwd']
-       v3= request.form['cpwd']
-       if v2 != v3:
-           erro= 'A palavra passe não coincide.'
-       else:
-           gravar (v1, v2, v3)
-    return render_template('registo.html',erro=erro )
-
+        v1 = request.form['usr']
+        v2 = request.form['pwd']
+        v3 = request.form['cpwd']
+        if v2 != v3:
+            erro = 'A palavra passe não coincide.'
+        else:
+            alterar(v1, v2)
+    return render_template('newpass.html', erro=erro)
 
 
 if __name__ == '__main__':
